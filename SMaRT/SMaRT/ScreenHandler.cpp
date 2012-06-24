39478df3c635e4e67b3b42843efa7bc681e3201c
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2012-06-24 19:10:03 +0200

    Wrote some code, cat >:)

--- SMaRT/SMaRT/ScreenHandler.cpp
+++ SMaRT/SMaRT/ScreenHandler.cpp
@@ -2,10 +2,22 @@
 
 ScreenHandler::ScreenHandler()
 {
-
+	mGameScreen = NULL;
+	mMenuScreen = NULL;
 }
 
 ScreenHandler::~ScreenHandler()
+{
+	delete mGameScreen;
+	delete mMenuScreen;
+}
+
+void ScreenHandler::Update()
+{
+
+}
+
+void ScreenHandler::SwapState()
 {
 
 }
--- SMaRT/SMaRT/ScreenHandler.h
+++ SMaRT/SMaRT/ScreenHandler.h
@@ -1,16 +1,25 @@
 #ifndef SCREENHANDLER_H
 #define SCREENHANDLER_H
 
+#include "BaseScreen.h"
+#include "GameScreen.h"
+#include "MenuScreen.h"
+
 class ScreenHandler
 {
 public:
 	ScreenHandler();
-	~ScreenHandler();
+	virtual ~ScreenHandler();
+
+	void Update();
+	void SwapScreenState();
 
 private:
 
 private:
-
+	BaseScreen* mBaseScreen;
+	GameScreen* mGameScreen;
+	MenuScreen* mMenuScreen;
 
 };
 
